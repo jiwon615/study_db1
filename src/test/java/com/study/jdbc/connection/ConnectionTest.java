bf4db0475433cfb175611dad130e652892fcd823
jiwon615 <seg615@naver.com>
2023-01-31 19:38:02 +0900

    test: connection pool 사용하는 예제

--- src/test/java/com/study/jdbc/connection/ConnectionTest.java
+++ src/test/java/com/study/jdbc/connection/ConnectionTest.java
@@ -1,6 +1,8 @@
 package com.study.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -25,6 +27,23 @@ public class ConnectionTest {
     @Test
     void dataSourceDriverManager() throws SQLException {
         DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    @Test
+    @DisplayName("DataSource를 통해 connection pool 사용하는 예제")
+    void dataSourceConnectionPool() throws SQLException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyTestPool");
+
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
         Connection conn1 = dataSource.getConnection();
         Connection conn2 = dataSource.getConnection();
         log.info("conn1={}, class={}", conn1, conn1.getClass());
