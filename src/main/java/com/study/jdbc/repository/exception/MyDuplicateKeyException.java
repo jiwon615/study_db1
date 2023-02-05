768c86f55a066a5d5b25e31049d990fd27d08c08
jiwon615 <seg615@naver.com>
2023-02-05 13:00:12 +0900

    feat: 데이터 접근 예외 직접 만들기

---
+++ src/main/java/com/study/jdbc/repository/exception/MyDuplicateKeyException.java
@@ -0,0 +1,22 @@
+package com.study.jdbc.repository.exception;
+
+/**
+ * 데이터 접근 예외 직접 만들기
+ */
+public class MyDuplicateKeyException extends MyDbException {
+
+    public MyDuplicateKeyException() {
+    }
+
+    public MyDuplicateKeyException(String message) {
+        super(message);
+    }
+
+    public MyDuplicateKeyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MyDuplicateKeyException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/test/java/com/study/jdbc/translator/ExceptionTranslatorV1Test.java
@@ -0,0 +1,97 @@
+package com.study.jdbc.translator;
+
+import com.study.jdbc.connection.ConnectionConstant;
+import com.study.jdbc.domain.Member;
+import com.study.jdbc.repository.exception.MyDbException;
+import com.study.jdbc.repository.exception.MyDuplicateKeyException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.JdbcUtils;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
+
+import static com.study.jdbc.connection.ConnectionConstant.*;
+
+@Slf4j
+public class ExceptionTranslatorV1Test {
+
+    Service service;
+    Repository repository;
+
+    @BeforeEach
+    void init() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new Repository(dataSource);
+        service = new Service(repository);
+    }
+
+    @Test
+    void duplicateKeySave() {
+        service.create("hello");
+        service.create("hello");
+    }
+
+    @Slf4j
+    @RequiredArgsConstructor
+    static class Service {
+        private final Repository repository;
+
+        public void create(String memberId) {
+            try {
+                repository.save(new Member(memberId, 0));
+            } catch (MyDuplicateKeyException e) {
+                log.info("키 중복, 복구 시도");
+                String retryId = generateNewId(memberId);
+                log.info("retryId={}", retryId);
+                repository.save(new Member(memberId, 0));
+            } catch (MyDbException e) {
+                log.info("데이터 접근 계층 예외", e);
+                throw e;
+            }
+        }
+
+        private String generateNewId(String memberId) {
+            return memberId + new Random().nextInt(1000);
+        }
+    }
+
+    @RequiredArgsConstructor
+    static class Repository {
+        private final DataSource dataSource;
+
+        public Member save(Member member) {
+            String sql = "insert into member(member_id, money) values(?, ?)";
+            Connection con = null;
+            PreparedStatement pstmt = null;
+
+            try {
+                con = dataSource.getConnection();
+                pstmt = con.prepareStatement(sql);
+                pstmt.setString(1, member.getMemberId());
+                pstmt.setInt(2, member.getMoney());
+                pstmt.executeUpdate();
+                return member;
+            } catch (SQLException e) {
+                /**
+                 * 여기서의 문제: errorCode는 각각의 데이터베이스마다 다름. 결과적으로 데이터베이스 변경될 때마다
+                 * errorCode 모두 변경해야힘 :(
+                 */
+                if (e.getErrorCode() == 23505) {
+                    throw new MyDuplicateKeyException(e);
+                }
+                throw new MyDbException(e);
+            } finally {
+                JdbcUtils.closeStatement(pstmt);
+                JdbcUtils.closeConnection(con);
+            }
+        }
+    }
+}
